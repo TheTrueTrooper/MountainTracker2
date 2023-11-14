f2f34a5404530206d2575efb20ecba44e8f6bf44
Angelo <BitSan@outlook.com>
2023-11-13 23:13:19 -0700

    add-open-street-maps: Added a basic component to get it running.
    Added a basic component to get it running.

--- MountainTracker.Client/src/app/app.component.ts
+++ MountainTracker.Client/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { OpenStreetMapModule, TargetOSMSourceFactory } from './open-street-map';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, OpenStreetMapModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'MountainTracker';
+
+  protected readonly mapSource = TargetOSMSourceFactory;
 }
---
+++ MountainTracker.Client/src/app/open-street-map/index.ts
@@ -0,0 +1,2 @@
+export * from "./OSMComponent/leaflet-map.component";
+export * from "./open-street-map.module";

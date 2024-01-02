09d484271c76fa5607f63a8ba9ec155a2d936744
Angelo <BitSan@outlook.com>
2024-01-01 18:08:02 -0700

    add-admin-region-editing: fixed root test
    add-admin-region-editing: fixed root test

--- MountainTracker.Client/src/app/app.component.spec.ts
+++ MountainTracker.Client/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './core-modules/core-module';
+import { MockComponent } from 'ng-mocks';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [MockComponent(MenuComponent)]
     }).compileComponents();
   });
 
@@ -18,12 +20,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('MountainTracker');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MountainTracker');
   });
 });
